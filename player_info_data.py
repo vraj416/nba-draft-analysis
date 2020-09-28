896eba786ff3246ffcf8081d0a52c4007ce77b10
vraj416 <vraj416@gmail.com>
2020-09-28 00:14:21 -0500

    got required columns

--- player_info_data.py
+++
@@ -1,6 +0,0 @@
-import pickle
-import pandas as pd 
-
-df = pd.read_pickle('player_info.pickle')
-
-print(df.columns)
---
+++ python_scripts/player_info_data.py
@@ -0,0 +1,10 @@
+import pickle
+import pandas as pd 
+
+df_player_info = pd.read_pickle('player_info.pickle')
+df_player_info= df_player_info[["PLAYER_ID", "PLAYER_NAME", "TEAM_ID", "TEAM_ABBREVIATION", "DRAFT_YEAR", "DRAFT_ROUND", "DRAFT_NUMBER"]]
+
+df_player_info['DRAFT_NUMBER'].replace(['Undrafted'], "61", inplace=True)
+df_player_info['DRAFT_ROUND'].replace(['Undrafted'], "3", inplace=True)
+
+print(df_player_info.head())
